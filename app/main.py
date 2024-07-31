41f51968f3785da7b19aa2db8283cc513c7f68b8
Jaxsbr <19993683+Jaxsbr@users.noreply.github.com>
2024-07-31 21:48:31 +0000

    shebang for linux, output path input mechanism

--- app/main.py
+++ app/main.py
@@ -1,13 +1,28 @@
+#!/usr/bin/env python3
+# NOTE: shebang makes this script work with my WSL setup
+
 from python_project.builder import Builder
 from python_project.interrogator import get_project_model
 
 def main():
     print('[==== Hello Mr Python developer ====]')
+
+    # TODO: read form a config file
+    output_path = '/mnt/c/code-lab/python_lab'
+    print(f'[--- Default Project Output Path ---]: {output_path}')
+    path_input = input('Y = Set project output path. N/Empty = Use default): ').strip()
+    if path_input == 'y':        
+        output_path = input('Enter the project name: ').strip()
+        if not output_path:
+            print('[==== Good bey ;) ====]')
+            return
+
     model = get_project_model()
     if not model:
         print('[==== Good bey ;) ====]')
+        return
 
-    builder = Builder('c:\\Users\\JacoBrink\\Desktop')
+    builder = Builder(output_path)
     print(f'project name: {model.project_name}')
     print(f'venv name: {model.venv_name}')
     builder.build(model)
